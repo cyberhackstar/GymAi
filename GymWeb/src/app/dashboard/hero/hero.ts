28af8b6fababd7daa463e9d02e8d960c7ad7a37a
bhawesh <sharma2001bhawesh@gmail.com>
2025-08-27 16:00:28 +0530

    `Refactored CSS and HTML files across the GymWeb application, including hero background image, mobile responsiveness, and link formatting.`

--- GymWeb/src/app/dashboard/hero/hero.ts
+++ GymWeb/src/app/dashboard/hero/hero.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [RouterModule],
+  imports: [NgOptimizedImage, RouterModule, FormsModule, CommonModule],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
-export class Hero {}
+export class Hero {
+  isMobile = window.innerWidth <= 768;
+
+  @HostListener('window:resize')
+  onResize() {
+    this.isMobile = window.innerWidth <= 768;
+  }
+}
