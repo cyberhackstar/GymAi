fde1dde9e445321014255d0bb4e828942c4d625b
bhawesh <sharma2001bhawesh@gmail.com>
2025-08-30 02:25:52 +0530

    `Update environment.prod.ts with new auth URL`

--- GymWeb/src/environments/environment.prod.ts
+++ GymWeb/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  authUrl: 'https://auth-service-gymai.neelahouse.cloud/api/auth',
+  authUrl: 'https://auth-service-gymai.neelahouse.cloud',
   userUrl: 'https://user-service-gymai.neelahouse.cloud/api/user',
   planUrl: 'https://plan-service-gymai.neelahouse.cloud/api/fitness',
 };
