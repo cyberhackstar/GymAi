da2838b775945e9a698b5bf0e617408be23f1e9a
bhawesh <sharma2001bhawesh@gmail.com>
2025-08-31 00:29:40 +0530

    `Remove Vercel Speed Insights and Analytics imports, replace with inject functions`

--- GymWeb/src/main.ts
+++ GymWeb/src/main.ts
@@ -1,7 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/next';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
+import { inject } from '@vercel/analytics';
+injectSpeedInsights();
+inject();
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
