427449086cc9ec3e6c3de20757bb5fe47528f74c
bhawesh <sharma2001bhawesh@gmail.com>
2026-03-25 22:50:39 +0530

    Remove Extra services

--- user-service/src/main/java/com/gymai/user_service/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.gymai.user_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
